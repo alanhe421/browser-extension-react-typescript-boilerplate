f90bff4b31a9efc30a380faa48fa63a52cd22146
alanhe <qianghe421@gmail.com>
2022-01-13 22:50:48 +0800

    支持插件外部发送的消息，比如网页

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,11 +1,20 @@
 // Listen to messages sent from other parts of the extension.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // onMessage must return "true" if response is async.
-    let isResponseAsync = false;
+  // onMessage must return "true" if response is async.
+  let isResponseAsync = false;
 
-    if (request.popupMounted) {
-        console.log('eventPage notified that popup.tsx has mounted.');
-    }
+  if (request.popupMounted) {
+    console.log('eventPage notified that popup.tsx has mounted.');
+  }
 
-    return isResponseAsync;
+  return isResponseAsync;
 });
+
+/**
+ * Fired when a message is sent from another extension/app
+ */
+chrome.runtime.onMessageExternal.addListener(
+  () => {
+
+  },
+);
