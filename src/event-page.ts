0586be3e0d2bca0b79f2db115d15d3aab656659c
alanhe <qianghe421@gmail.com>
2022-01-20 23:24:03 +0800

    Update externally_connectable

--- src/event-page.ts
+++ src/event-page.ts
@@ -14,7 +14,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
  * Fired when a message is sent from another extension/app
  */
 chrome.runtime.onMessageExternal.addListener(
-  () => {
-
+  (message, sender, sendResponse) => {
+    console.log('received!');
+    chrome.windows.create(
+      {
+        url: `popup.html`,
+        type: 'popup',
+        height: 640,
+        width: 357,
+        left: 100,
+        top: 100,
+        focused: true,
+      },
+    );
+    sendResponse({});
+    return true;
   },
 );
