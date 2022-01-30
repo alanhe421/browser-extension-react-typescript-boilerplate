be389f7fd211bea0474e1893274cf7be420fd78f
Alan He <qianghe421@gmail.com>
2022-01-30 13:43:53 +0800

    fixed #8

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,13 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const ExtensionReloader = require('webpack-extension-reloader');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map"
+  mode: "development", devtool: "inline-source-map", plugins: [new ExtensionReloader({
+    port: 9090, // Which port use to create the server
+    reloadPage: true, // Force the reload of the page also
+    entries: {
+      popup: "popup", background: "eventPage", contentScript: "contentScript"
+    }
+  }),]
 });
