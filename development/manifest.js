24b44ce03fdf057628a09bc5df182b0afa135c44
Alan He <qianghe421@gmail.com>
2022-03-21 15:14:03 +0800

    feat: 支持不同浏览器打包，留口子

--- development/manifest.js
+++ development/manifest.js
@@ -1,11 +1,19 @@
-const manifestJSON = require(`${__dirname}/../src/manifest/manifest.json`);
 const fs = require('fs');
 const path = require('path');
+const yargs = require('yargs/yargs')
+const {hideBin} = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv;
+
 const packageJSON = require(`${__dirname}/../package.json`);
+
+
+const baseJSON = require(`${__dirname}/../src/manifest/base.json`);
+const platformJSON = require(`${__dirname}/../src/manifest/${argv.platform || 'chrome'}.json`);
+
 /**
  * 版本采用package.json中配置值
  */
 const file = path.join(__dirname, `/../dist/manifest.json`);
 fs.writeFileSync(file, JSON.stringify({
-  ...manifestJSON, version: packageJSON.version
-}), {encoding: 'utf8'});
+  ...baseJSON, ...platformJSON, version: packageJSON.version
+}, null, 4), {encoding: 'utf8'});
